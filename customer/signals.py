330fe097481472d4daa0403d97d924c15c0508fe
Priyal267 <priyalsinghal11@gmail.com>
2024-07-16 09:54:37 -0400

    done

--- customer/signals.py
+++ customer/signals.py
@@ -8,11 +8,28 @@ import ipdb
 def create_transfer_transaction(sender, instance, created, **kwargs):
     if created and instance.transaction_type == 'Transfer':
         
-        ipdb.set_trace()
+        ipdb.set_trace() 
         from_account = instance.account_no
-        TransferTransactions.objects.create(
-            from_account_no=from_account,
-            to_account_no=instance.account_no.id,  # Replace with logic to find the correct 'to_account' transaction
+        from_account_no = from_account.account_no
+        
+        recipient_member_id = instance.member_id
+        
+        
+        try:
+            to_account = SavingAccount.objects.get(member=recipient_member_id)
+        except SavingAccount.DoesNotExist:
+            raise ValueError(f"SavingAccount with member ID {recipient_member_id} does not exist.")
+        
+          # Assuming you have a field in Transactions called recipient_member_id
+        # recipient_member_id = instance.recipient_member_id  # Replace with your actual field
+        
+        # Fetch the recipient's SavingAccount based on recipient_member_id
+        # to_account = SavingAccount.objects.get(member=recipient_member_id)
+        to_account_no = to_account.account_no
+        TransferTransactions.objects.create( 
+            from_account_no=from_account_no,
+            to_account_no=to_account_no,
+            # to_account_no=instance.account_no.id,  # Replace with logic to find the correct 'to_account' transaction
             amount=instance.amount,
             description=instance.description
         )
