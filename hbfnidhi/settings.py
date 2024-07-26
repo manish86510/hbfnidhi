0f7b03b5c6158b920d27bf329c7512025602366c
Priyal267 <priyalsinghal11@gmail.com>
2024-07-26 10:45:47 -0400

    mail

--- hbfnidhi/settings.py
+++ hbfnidhi/settings.py
@@ -147,7 +147,7 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'bigrock.titan.email'  # Replace with your SMTP server
 EMAIL_PORT = 587  # Common port for TLS
 EMAIL_USE_TLS = True
